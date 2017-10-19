73e86d18bfc94a7ac86da6ab9fe1148e800bcfbd
Risitop <aziz.foucheasnoun@gmail.com>
2017-10-20 01:38:46 +0200

    ArticleManager, HTTPManager, Stop criteri

--- Recommendation/recommendation.js
+++ Recommendation/recommendation.js
@@ -18,6 +18,8 @@ function ArticleManager() {
 
   this.addArticle = function(title, views, url) {
 
+    HM.decountArticle();
+
     if (Object.keys(this.articles).length > MAX_ARTICLES) {
       let minValue = Infinity, minKey = null;
       for (let article in this.articles) {
@@ -31,7 +33,76 @@ function ArticleManager() {
       } else return;
     }
     this.articles[title] = {'views': views, 'url': url};
-    console.log(this);
+  };
+
+  this.getLinks = function(page) { // Get pertinent links of a wikipedia page
+
+    // We get each paragraph of the page in an array
+    let paragraphs = page.getElementsByClassName("mw-parser-output")[0].getElementsByTagName("p");
+
+    // We extract every links from paragraphs
+    let links = [];
+    for (let i=0; i < paragraphs.length; ++i) {
+        let tmp_links = paragraphs[i].getElementsByTagName("a");
+        for (let j=0; j < tmp_links.length; ++j) {
+            if ( !["new", "mw-redirect"].includes(tmp_links[j].className)) {
+              links.push(tmp_links[j]);
+            }
+        }
+    }
+
+    // We keep pertinent links
+    let goodLinks = [];
+    for (let i=0; i < links.length; ++i) {
+      let text = links[i].innerHTML; // We get the text associated to the link
+      let excluder = new RegExp(/^.*[\[<].*|[\d-_]+$/, 'i'); // We detect dates, imgs, isbn...
+      if (!excluder.exec(text)) { goodLinks.push(links[i]); } // And we add the others
+    }
+
+    return goodLinks;
+  };
+
+  this.getViews = function(pageTitle, pageUrl, dateBeg, dateEnd) { // Get views of a given page. Date format : AAAAMMDD
+    pageTitle.replace("/", "_");
+    let replaceRegExp = /^(.*)(#1)(.*)(#2)(\/)(#3)+/; // Regexp to formate getviews url from api
+    try {
+      let that = this;
+      HM.httpGetAsync(
+        viewsUrl.replace(replaceRegExp, "$1" + pageTitle + "$3" + dateBeg + "/" + dateEnd ),
+          function(responseJSON) { that.addArticle(pageTitle, responseJSON.views, pageUrl); }
+      );
+    } catch (error) {
+      console.warn(error);
+    }
+  };
+}
+
+function HTTPManager() {
+  this.requestsThrown = 0;
+
+  this.httpGetAsync = function(theUrl, callback)
+  {
+    try {
+      this.requestsThrown++;
+      let xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() {
+          if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
+              callback(JSON.parse(xmlHttp.responseText).items[0]);
+      };
+      xmlHttp.open("GET", theUrl, true); // true for asynchronous
+      xmlHttp.send(null);
+    } catch (error) {
+      this.decountArticle();
+      throw error;
+    }
+  };
+
+  this.decountArticle = function() {
+    this.requestsThrown--;
+
+    if (this.requestsThrown == 0) {
+      console.log(AM);
+    }
   };
 }
 
@@ -39,67 +110,13 @@ function ArticleManager() {
 
 let viewsUrl = 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/#1/daily/#2/#3';
 let AM = new ArticleManager();
+let HM = new HTTPManager();
 
-function httpGetAsync(theUrl, callback)
-{
-  try {
-    let xmlHttp = new XMLHttpRequest();
-    xmlHttp.onreadystatechange = function() {
-        if (xmlHttp.readyState == 4 && xmlHttp.status == 200)
-            callback(JSON.parse(xmlHttp.responseText).items[0]);
-    };
-    xmlHttp.open("GET", theUrl, true); // true for asynchronous
-    xmlHttp.send(null);
-  } catch (error) {
-    throw error;
-  }
-}
-
-function getViews(pageTitle, pageUrl, dateBeg, dateEnd) { // Get views of a given page. Date format : AAAAMMDD
-  pageTitle.replace("/", "_");
-  let replaceRegExp = /^(.*)(#1)(.*)(#2)(\/)(#3)+/; // Regexp to formate getviews url from api
-  try {
-    httpGetAsync(
-      viewsUrl.replace(replaceRegExp, "$1" + pageTitle + "$3" + dateBeg + "/" + dateEnd ),
-        function(responseJSON) { AM.addArticle(pageTitle, responseJSON.views, pageUrl); }
-    );
-  } catch (error) {
-    console.warn(error);
-  }
-}
-
-function getLinks(page) { // Get pertinent links of a wikipedia page
-
-  // We get each paragraph of the page in an array
-  let paragraphs = page.getElementsByClassName("mw-parser-output")[0].getElementsByTagName("p");
-
-  // We extract every links from paragraphs
-  let links = [];
-  for (let i=0; i < paragraphs.length; ++i) {
-      let tmp_links = paragraphs[i].getElementsByTagName("a");
-      for (let j=0; j < tmp_links.length; ++j) {
-          if ( !["new", "mw-redirect"].includes(tmp_links[j].className)) {
-            links.push(tmp_links[j]);
-          }
-      }
-  }
-
-  // We keep pertinent links
-  let goodLinks = [];
-  for (let i=0; i < links.length; ++i) {
-    let text = links[i].innerHTML; // We get the text associated to the link
-    let excluder = new RegExp(/^.*[\[<].*|[\d-_]+$/, 'i'); // We detect dates, imgs, isbn...
-    if (!excluder.exec(text)) { goodLinks.push(links[i]); } // And we add the others
-  }
-
-
-  return goodLinks;
-}
 
 (function() {
     'use strict';
 
-    for (let l of getLinks(document)) {
-      getViews(l.title, l.href, "20171018", "20171019");
+    for (let l of AM.getLinks(document)) {
+      AM.getViews(l.title, l.href, "20171018", "20171019");
     }
 })();
