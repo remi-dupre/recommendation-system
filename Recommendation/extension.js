e5c14bbadea67e0974ea33594c9a092da999cf51
youkad <youqad@gmail.com>
2017-10-27 01:07:40 +0200

    Fixed p5 and CSS links

--- Recommendation/extension.js
+++ Recommendation/extension.js
@@ -1,22 +1,23 @@
 // ==UserScript==
-// @name         Wikipedia Recommendation
+// @name         Wikiex
 // @namespace    http://tampermonkey.net/
 // @version      0.1
-// @description  try to take over the world!
+// @description  Wikipedia Recommender System
 // @author       You
 // @match        https://en.wikipedia.org/wiki/*
-// @require 	 <script src="../p5.min.js"></script>
-// @require 	 <script src="../addons/p5.dom.min.js"></script>
-// @require 	 <script src="../addons/p5.sound.min.js"></script>
-// @require 	 <script src="sketch.js"></script>
-// @require 	 <link rel="stylesheet" href="styles.css">
+// @require 	 https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/p5.min.js
+// @require 	 https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/addons/p5.dom.min.js
+// @require 	 https://cdnjs.cloudflare.com/ajax/libs/p5.js/0.5.16/addons/p5.sound.min.js
+// @resource     customCSS https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/styles.css
 // @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/userinterface.js
-// @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/styles.css
 // @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/Constants.js
 // @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/HttpModule.js
 // @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/APIModule.js
 // @require      https://raw.githubusercontent.com/remi-dupre/recommendation-system/master/Recommendation/api.js
-// @grant        none
+// @grant        GM_addStyle
+// @grant        GM_getResourceText
 // ==/UserScript==
 
-/////////////////////////////////////////////////////////
+var newCSS = GM_getResourceText ("customCSS");
+GM_addStyle (newCSS);
+_
