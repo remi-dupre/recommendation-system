8893c10791950a87f6156b58c0e1729174bf0b6d
fnareoh <garance.gourdel@laposte.net>
2017-11-19 14:51:34 +0100

    Word to vect categories

--- Recommendation/api.js
+++ Recommendation/api.js
@@ -7,10 +7,11 @@ const testArticle = {
 };
 
 // apiMod.getViews(testArticle);
-// apiMod.getAttr(14995351, 'categories', console.log);
+ //apiMod.getAttr(14995351, 'categories', console.log);
 // apiMod.getAttr(16095, 'categories', console.log);
 
 apiMod.distance(14995351, 16095,  (a) => console.log('Distance Michael - Jimi : ', a));
 apiMod.distance(14995351, 5843419, (a) => console.log('Distance Michael - France : ', a));
 apiMod.distance(17515, 5843419, (a) => console.log('Distance Luxembourg - France : ', a));
 apiMod.distance(22989, 5843419, (a) => console.log('Distance Paris - France : ', a));
+apiMod.getAttribute(47527969, 'categories', (a) => console.log('categories word to vect :',a));
