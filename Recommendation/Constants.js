0aeebb34ffc26994dcc366b9219c5435cab071d8
Risitop <aziz.foucheasnoun@gmail.com>
2017-11-23 14:45:43 +0100

    Update Constants.js

--- Recommendation/Constants.js
+++ Recommendation/Constants.js
@@ -3,9 +3,11 @@ const now = new Date();
 const Constants = {
     REQUESTS_MAX : 200,
     PEDIA_URL : 'https://[[LANG]].wikipedia.org/w/api.php',
+    BASE_URL : 'https://en.wikipedia.org/wiki/',
     VIEWS_URL : 'https://wikimedia.org/api/rest_v1/metrics/pageviews/per-article/en.wikipedia/all-access/all-agents/#1/daily/#2/#3',
+    MOST_VIEWED_URL: 'https://wikimedia.org/api/rest_v1/metrics/pageviews/top/en.wikipedia/all-access/',
     STORAGE_ARTICLES : 'WikiRec|articles',
-
+    
     //Slideshow
     FADE_SPEED : 0.03,
     FREQUENCY : 16,
@@ -16,4 +18,8 @@ const Constants = {
 
     // Filter max selections
     SECOND_PASS_CANDIDATES: 20
+
+    // Most viewed articles _imagesCount
+    MOST_VIEWED_COUNT: 50,
+    MOST_VIEWED_TOTAL: 1000
 };
