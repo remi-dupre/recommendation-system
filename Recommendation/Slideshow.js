2b70f39784359e71b3a9ef6682bd5b5de90218af
youkad <youqad@gmail.com>
2017-11-25 22:59:01 +0100

    fix

--- Recommendation/Slideshow.js
+++ Recommendation/Slideshow.js
@@ -22,7 +22,7 @@ class Slideshow {
             if (!String.prototype.format) {
               String.prototype.format = function() {
                 var args = arguments;
-                return that.replace(/{(\d+)}/g, function(match, number) {
+                return this.replace(/{(\d+)}/g, function(match, number) {
                   return typeof args[number] != 'undefined'
                     ? args[number]
                     : match
