fe3e214ab841c05081f0e6f73d26f95ba30c6346
youkad <youqad@gmail.com>
2017-11-26 01:28:23 +0100

    fix

--- Recommendation/Slideshow.js
+++ Recommendation/Slideshow.js
@@ -51,7 +51,7 @@ class Slideshow {
                 $(that._HTMLelement).find('.carousel-inner .row:last')
                         .append('<div class="bootstrap col-md-4">\
                               <figure class="bootstrap gallery-item">\
-                                  <a href="{0}"><div class="bootstrap img-responsive thumbnail img" style="background-image:url(\'{1}\'); overflow: hidden;"></div>\
+                                  <a href="{0}"><div class="bootstrap img-responsive thumbnail img" style="background-image:url(\'{1}\'); overflow: hidden; background-position: center; background-size: cover;"></div>\
                                   <figcaption class="bootstrap img-title" style="visibility: hidden;">\
                                       <h5>{2}</h5>\
                                   </figcaption>\
